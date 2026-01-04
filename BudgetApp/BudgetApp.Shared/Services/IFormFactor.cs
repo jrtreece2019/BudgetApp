567a31f52125fcd783a8cca62741238186ca98e2
Jonah <jonaht@kellis.com>
2026-01-03 21:11:49 -0600

    Initial commit: BudgetApp MAUI project

---
+++ BudgetApp/BudgetApp.Shared/Services/IFormFactor.cs
@@ -0,0 +1,8 @@
+namespace BudgetApp.Shared.Services
+{
+    public interface IFormFactor
+    {
+        public string GetFormFactor();
+        public string GetPlatform();
+    }
+}
---
+++ BudgetApp/BudgetApp.Web/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using BudgetApp.Shared.Services;
+
+namespace BudgetApp.Web.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return "Web";
+        }
+
+        public string GetPlatform()
+        {
+            return Environment.OSVersion.ToString();
+        }
+    }
+}
